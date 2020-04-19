b7a21e5190b77285323ed1b9494e86ae488685d9
Wicho <luiscalderonmiranda@gmail.com>
2020-04-18 23:21:13 -0500

    Still working in input for exercises and time, function to print array working properly

--- calculadoraCalorias.c
+++ calculadoraCalorias.c
@@ -5,24 +5,25 @@
 /*TODO: add random number to calorias array indexes 4, 15
  * 		length functions working only leaving length for the elements inside the array of pointers 
  * 		use sizeof(pointerarray) / sizeof(*ponterarray); 
- * 		DONE.Remember:Increse array size by 1 for '\0' to exist otherwise it seems to create a conflict for length function*/
+ * 		
+ * 	DONE.Remember:Increse array size by 1 for '\0' to exist otherwise it seems to create a conflict for length function*/
 
 int main(){
 	//double calorias[19] = {178, 258, 150, 130, 150, 108, 249, 255, 314, 363, 270, 378, 315, 325, 210, 300, 250, 261, 93}, quemadas;
-	//char* ejercicios[19] = {"Aerobic", "Baloncesto", "Bicicleta", "Bailar", "Jardineria", "Golf", "Hockey", "Montar Caballo", "Patinar sobre hielo", "Judo", "Montanierismo", "Remar",
-							//"Patinar Sobre Ruedas", "Correr", "Bajar Escaleras", "Subir Escaleras", "Nadar", "Tenis", "Voleibol"};
+	char* ejercicios[19] = {"Aerobic", "Baloncesto", "Bicicleta", "Bailar", "Jardineria", "Golf", "Hockey", "Montar Caballo", "Patinar sobre hielo", "Judo", "Montanierismo", "Remar",
+							"Patinar Sobre Ruedas", "Correr", "Bajar Escaleras", "Subir Escaleras", "Nadar", "Tenis", "Voleibol"};
 	char* dias[7] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};	
 	char* diasElegidos[7];
 	int diasELen;			
-	//int rutinaE[19], duracion[19], semanas[4];
-	
-	//printf("Length: %d\n", length(dias));
+	//int rutinaE[19];
+	int duracion[19];
+	//int semanas[4];
 	
 	diasELen = diasConRutina(dias, diasElegidos);
 	
-	//printf("Lenght: %d\n", length(diasElegidos));
+	printf("I'm out");
 	
-	//obtenerRutina();
+	obtenerRutina(diasElegidos,ejercicios,duracion,diasELen);
 	
 	//calcularCalorias();
 	
--- calculadoraCalorias.h
+++ calculadoraCalorias.h
@@ -8,9 +8,9 @@ int length(char* s[]){
 	return i;
 }
 
-void mostrarDias(char* dias[]){
-	for(int i = 0; i < 7; i++)
-		printf("%d.- %s\n", i + 1, dias[i]);
+void showArray(char* s[], int len){
+	for(int i = 0; i < len; i++)
+		printf("%d.- %s\n", i + 1, s[i]);
 }
 
 void eraseString(char* str[], int len, int position){
@@ -23,14 +23,26 @@ void eraseString(char* str[], int len, int position){
 int diasConRutina(char* dias[], char* diasElegidos[]){
 	int i = 1, x = 0;
 	printf("Selecciona los dias de la semana en los que realizas la actividad\npresiona 0 para ir a las actividades\n");
-	mostrarDias(dias);
+	showArray(dias,7);
 	while(i > 0){
 		scanf("%d",&i);
+		if(i == 0){
+			break;
+		}
 		diasElegidos[x] = dias[i-1];
 		if(*diasElegidos[x] != '\0'){
+			printf("El Valor de x es %d\n", x);
 			printf("Dias Elegidos: %s\n", diasElegidos[x++]);
 		}
 	}
 	return x--;
+}
+//Get the input for time and name of the exercise for each day. len - 1
+void obtenerRutina(char* dia[], char* ejercicios[], int tiempo[], int dialen){
+	printf("Si llego");
+	for(int i = 0; i < dialen; i++){
+		printf("Selecciona los ejercios que realices el dia %s\n", dia[i]);
+		showArray(ejercicios, 19);
+	}
 }
 
